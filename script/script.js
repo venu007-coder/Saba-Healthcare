439c3b69da19dcbffe8a65066b5fa15fc6967521
vcidex <127820522+venu007-coder@users.noreply.github.com>
2025-03-14 17:37:36 +0530

    5th file move
    index page update

--- script/script.js
+++ script/script.js
@@ -21,3 +21,84 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }, 20);
 });
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var calendarEl = document.getElementById('calendar');
+
+//     var calendar = new FullCalendar.Calendar(calendarEl, {
+//         initialView: 'dayGridMonth',  // Default view
+//         selectable: true,             // Allow date selection
+//         // events: [                      // Sample events
+//         //     {
+//         //         title: 'Meeting',
+//         //         start: '2025-03-06'
+//         //     },
+//         //     {
+//         //         title: 'Holiday',
+//         //         start: '2025-03-10',
+//         //         end: '2025-03-12'
+//         //     }
+//         // ],
+//         headerToolbar: {
+//     left: 'prev,next today',
+//     right: 'title',
+//     // center: 'customTitle',
+//   },
+//         dateClick: function(info) {    // Event on date click
+//             alert('Clicked on: ' + info.dateStr);
+//         },
+
+//         dayHeaderContent: function(arg) {
+//     // Use `arg.date` to get the date object and format it as needed
+//     const shortNames = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+//     return shortNames[arg.date.getDay()];  // Get the corresponding short day name
+// },
+// customButtons: {
+//     customTitle: {
+//         text: 'Book a Appointment',
+//     }
+// }
+//     });
+
+//     calendar.render();
+// });
+
+
+    // document.addEventListener('DOMContentLoaded', function () {
+    //     var calendarEl = document.getElementById('calendar');
+    //     var calendar = new FullCalendar.Calendar(calendarEl, {
+    //         initialView: 'dayGridMonth', // Default view
+    //         selectable: true, // Allow date selection
+    //         headerToolbar: {
+    //             left: 'prev,next today',
+    //             right: 'title',
+    //         },
+    //         dateClick: function (info) {
+    //             // Get the selected date
+    //             document.getElementById("appointmentDate").value = info.dateStr;
+
+              
+
+    //             // Show Bootstrap Modal
+    //             var appointmentModal = new bootstrap.Modal(document.getElementById('appointmentModal'));
+    //             appointmentModal.show();
+    //         }
+    //     });
+
+    //     calendar.render();
+    // });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("scroll", function () {
+        let navbar = document.querySelector(".navbar");
+        if (window.scrollY > 50) {
+            navbar.classList.add("scrolled"); // Add class when scrolled
+        } else {
+            navbar.classList.remove("scrolled"); // Remove class when at top
+        }
+    });
+});
+
+
