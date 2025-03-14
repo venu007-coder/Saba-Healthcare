1c97660208cd71f36c6233710320930b16c221d1
vcidex <127820522+venu007-coder@users.noreply.github.com>
2025-03-14 17:46:35 +0530

    6th change

---
+++ css/data.js
@@ -0,0 +1,6 @@
+// This is the array you want to use
+const users = [
+    { "name": "John", "age": 25, "city": "New York" },
+    { "name": "Alice", "age": 30, "city": "Los Angeles" },
+    { "name": "Bob", "age": 22, "city": "Chicago" }
+];
